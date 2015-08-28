d15c05d0cc27a38754dcf875677f51ee14f57828
Bryan Crowe <bryan@bryan-crowe.com>
2015-08-28 09:36:58 -0400

    Test default pagination response setting

--- tests/ApiPaginationComponentTest.php
+++ tests/ApiPaginationComponentTest.php
@@ -5,16 +5,28 @@ use BryanCrowe\ApiPagination\Controller\Component\ApiPaginationComponent;
 use Cake\Controller\ComponentRegistry;
 use Cake\Controller\Controller;
 use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use Cake\Network\Response;
+use Cake\Routing\DispatcherFactory;
+use Cake\Routing\Router;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+
+
+class ArticlesController extends Controller
+{
+    public $components = ['Paginator'];
+}
 
 /**
  * ApiPaginationComponentTest class
  */
 class ApiPaginationComponentTest extends TestCase
 {
+    public $fixtures = ['plugin.BryanCrowe/ApiPagination.Articles'];
+
     /**
      * setUp method
      *
@@ -22,6 +34,7 @@ class ApiPaginationComponentTest extends TestCase
      */
     public function setUp()
     {
+        $this->Articles = TableRegistry::get('BryanCrowe/ApiPagination.Articles', ['table' => 'bryancrowe_articles']);
         parent::setUp();
     }
 
@@ -37,12 +50,43 @@ class ApiPaginationComponentTest extends TestCase
 
     public function testNonApiPaginatedRequest()
     {
-        $request = new Request('/');
+        $request = new Request('/articles');
         $response = $this->getMock('Cake\Network\Response');
-        $controller = new Controller($request, $response);
+        $controller = new ArticlesController($request, $response);
         $apiPaginationComponent = new ApiPaginationComponent($controller->components());
         $event = new Event('Controller.beforeRender', $controller);
 
         $this->assertNull($apiPaginationComponent->beforeRender($event));
+    }
+
+    public function testDefaultPaginationSettings()
+    {
+        $request = new Request('/articles');
+        $request->env('HTTP_ACCEPT', 'application/json');
+        $response = $this->getMock('Cake\Network\Response');
+        $controller = new ArticlesController($request, $response);
+        $controller->set('data', $controller->paginate($this->Articles));
+        $apiPaginationComponent = new ApiPaginationComponent($controller->components());
+        $event = new Event('Controller.beforeRender', $controller);
+        $apiPaginationComponent->beforeRender($event);
+
+        $result = $apiPaginationComponent->_registry->getController()->viewVars['pagination'];
+        $expected = [
+            'finder' => 'all',
+            'page' => 1,
+            'current' => 20,
+            'count' => 23,
+            'perPage' => 20,
+            'prevPage' => false,
+            'nextPage' => true,
+            'pageCount' => 2,
+            'sort' => null,
+            'direction' => false,
+            'limit' => null,
+            'sortDefault' => false,
+            'directionDefault' => false
+        ];
+
+        $this->assertSame($expected, $result);
     }
 }
---
+++ tests/Fixture/ArticlesFixture.php
@@ -0,0 +1,42 @@
+<?php
+namespace BryanCrowe\ApiPagination\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class ArticlesFixture extends TestFixture
+{
+    public $table = 'bryancrowe_articles';
+
+    public $fields = [
+        'id' => ['type' => 'integer'],
+        'title' => ['type' => 'string', 'null' => false],
+        'body' => 'text',
+        '_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]]
+    ];
+
+    public $records = [
+        ['title' => 'Post #1', 'body' => 'This is the article body.'],
+        ['title' => 'Post #2', 'body' => 'This is the article body.'],
+        ['title' => 'Post #3', 'body' => 'This is the article body.'],
+        ['title' => 'Post #4', 'body' => 'This is the article body.'],
+        ['title' => 'Post #5', 'body' => 'This is the article body.'],
+        ['title' => 'Post #6', 'body' => 'This is the article body.'],
+        ['title' => 'Post #7', 'body' => 'This is the article body.'],
+        ['title' => 'Post #8', 'body' => 'This is the article body.'],
+        ['title' => 'Post #9', 'body' => 'This is the article body.'],
+        ['title' => 'Post #10', 'body' => 'This is the article body.'],
+        ['title' => 'Post #11', 'body' => 'This is the article body.'],
+        ['title' => 'Post #12', 'body' => 'This is the article body.'],
+        ['title' => 'Post #13', 'body' => 'This is the article body.'],
+        ['title' => 'Post #14', 'body' => 'This is the article body.'],
+        ['title' => 'Post #15', 'body' => 'This is the article body.'],
+        ['title' => 'Post #16', 'body' => 'This is the article body.'],
+        ['title' => 'Post #17', 'body' => 'This is the article body.'],
+        ['title' => 'Post #18', 'body' => 'This is the article body.'],
+        ['title' => 'Post #19', 'body' => 'This is the article body.'],
+        ['title' => 'Post #20', 'body' => 'This is the article body.'],
+        ['title' => 'Post #21', 'body' => 'This is the article body.'],
+        ['title' => 'Post #22', 'body' => 'This is the article body.'],
+        ['title' => 'Post #23', 'body' => 'This is the article body.']
+    ];
+}
